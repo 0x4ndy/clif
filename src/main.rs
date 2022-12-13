d8dcf5ea016891a5a9af9e36a98ce40e71525b55
Andy <andy@andy.codes>
2022-12-13 08:48:53 +0000

    Split into main and lib parts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::process;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    if let Err(e) = clif::run() {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+    Ok(())
 }
